dd98826f6f8f6629cb92d34a876a185f0a483719
zannatul <jannatulfahmida15@gmail.com>
2023-11-16 21:00:02 +0600

    products section added

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,21 @@
-import Image from 'next/image'
+import HomeBanner from "./components/HomeBanner";
+import Container from "./components/Container";
+import { products } from "@/utils/products";
+import ProductCard from "./components/products/ProductCard";
 
 export default function Home() {
   return (
-    <div>E Shop</div>
-  )
+    <div className="p-8">
+      <Container>
+        <HomeBanner />
+        <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8">
+          {
+            products.map((product: any)=>{
+              return <ProductCard key={product.id} data={product} />
+            })
+          }
+        </div>
+      </Container>
+    </div>
+  );
 }
