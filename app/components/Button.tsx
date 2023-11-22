9e5944f0f14b92e10c1f95851df17a3ad82f2ca8
zannatul <jannatulfahmida15@gmail.com>
2023-11-22 21:36:11 +0600

    product details page updated

---
+++ app/components/Button.tsx
@@ -0,0 +1,54 @@
+"use client";
+
+import { IconType } from "react-icons";
+
+interface ButtonProps {
+  label: string;
+  disabled?: boolean;
+  outline?: boolean;
+  small?: boolean;
+  custom?: string;
+  icon?: IconType;
+  onClick: (e: React.MouseEvent<HTMLButtonElement>) => void;
+}
+
+const Button: React.FC<ButtonProps> = ({
+  label,
+  disabled,
+  outline,
+  small,
+  custom,
+  icon: Icon,
+  onClick,
+}) => {
+  return (
+    <button
+      disabled={disabled}
+      className={`
+    disabled:opacity-70
+    disabled:cursor-not-allowed
+    rounded-md
+    hover:opacity-80
+    transition
+    w-full
+    border-purple-700
+    flex
+    items-center
+    justify-center
+    gap-2
+    ${outline ? "bg-white text-purple-700" : "bg-purple-700 text-white"}
+    ${
+      small
+        ? "text-sm font-light py-1 px-2 border-[1px]"
+        : "text-md font-semibold py-3 px-4 border-2"
+    }
+    ${custom ? custom : ""}
+    `}
+    >
+      {Icon && <Icon size={24} />}
+      {label}
+    </button>
+  );
+};
+
+export default Button;
--- app/product/[productId]/ProductDetails.tsx
+++ app/product/[productId]/ProductDetails.tsx
@@ -1,22 +1,82 @@
 "use client";
 
+import Button from "@/app/components/Button";
+import ProductImage from "@/app/components/products/ProductImage";
+import SetColor from "@/app/components/products/SetColor";
+import SetQuantity from "@/app/components/products/SetQuantity";
 import { Rating } from "@mui/material";
+import { useCallback, useState } from "react";
 
 interface ProductDetailsProps {
   product: any;
 }
+
+export type CartProductType = {
+  id: string;
+  name: string;
+  description: string;
+  category: string;
+  brand: string;
+  selectedImg: SelectedImgType;
+  quantity: number;
+  price: number;
+};
+
+export type SelectedImgType = {
+  color: string;
+  colorCode: string;
+  image: string;
+};
 
 const Horizontal = () => {
   return <hr className="w-[80%] my-2 border-sky-500" />;
 };
 
 const ProductDetails: React.FC<ProductDetailsProps> = ({ product }) => {
+  const [cartProduct, setCartProduct] = useState<CartProductType>({
+    id: product.id,
+    name: product.name,
+    description: product.description,
+    category: product.category,
+    brand: product.brand,
+    selectedImg: { ...product.images[0] },
+    quantity: 1,
+    price: product.price,
+  });
+
   const productRating =
     product.reviews.reduce((acc: number, item: any) => item.rating + acc, 0) /
     product.reviews.length;
+
+  const handleColorSelect = useCallback(
+    (value: SelectedImgType) => {
+      setCartProduct((prev) => {
+        return { ...prev, selectedImg: value };
+      });
+    },
+    [cartProduct.selectedImg]
+  );
+
+  const handleQtyIncrease = useCallback(() => {
+    if (cartProduct.quantity === 99) {
+      return;
+    }
+    setCartProduct((prev) => {
+      return { ...prev, quantity: prev.quantity + 1 };
+    });
+  }, [cartProduct]);
+  const handleQtyDecrease = useCallback(() => {
+    if (cartProduct.quantity === 1) {
+      return;
+    }
+    setCartProduct((prev) => {
+      return { ...prev, quantity: prev.quantity - 1 };
+    });
+  }, [cartProduct]);
+
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 gap-12">
-      <div>images</div>
+      <ProductImage cartProduct={cartProduct} product={product} handleColorSelect={handleColorSelect} />
       <div className="flex flex-col gap-1 text-slate-500 text-sm">
         <h2 className="text-3xl font-medium text-slate-700">{product.name}</h2>
         <div className="flex items-center gap-2">
@@ -32,13 +92,32 @@ const ProductDetails: React.FC<ProductDetailsProps> = ({ product }) => {
         <div>
           <span className="font-semibold">BRAND:</span> {product.brand}
         </div>
-        <div className={`${product.inStock ? 'text-green-600' : 'text-rose-600'} font-semibold`}>{product.inStock ? 'In Stock': 'Out Of Stock'}</div>
+        <div
+          className={`${
+            product.inStock ? "text-green-600" : "text-rose-600"
+          } font-semibold`}
+        >
+          {product.inStock ? "In Stock" : "Out Of Stock"}
+        </div>
         <Horizontal />
-        <div>color</div>
+        <SetColor
+          cartProduct={cartProduct}
+          images={product.images}
+          handColorSelect={handleColorSelect}
+        />
         <Horizontal />
-        <div>quantity</div>
+        <SetQuantity
+          cartProduct={cartProduct}
+          handleQtyIncrease={handleQtyIncrease}
+          handleQtyDecrease={handleQtyDecrease}
+        />
         <Horizontal />
-        <div>Add To Cart</div>
+        <div className="max-w-[300px]">
+          <Button
+          label="Add To Cart"
+          onClick={()=>{}}
+          />
+        </div>
       </div>
     </div>
   );
