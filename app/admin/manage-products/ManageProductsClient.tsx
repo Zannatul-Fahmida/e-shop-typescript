45eb67014e577de4aa3f9e6111339d7cef3613db
zannatul <jannatulfahmida15@gmail.com>
2024-01-09 21:37:29 +0600

    manage product page done

--- app/admin/manage-products/ManageProductsClient.tsx
+++ app/admin/manage-products/ManageProductsClient.tsx
@@ -4,6 +4,21 @@ import { Product } from "@prisma/client";
 import { DataGrid, GridColDef } from "@mui/x-data-grid";
 import formatPrice from "@/utils/formatPrice";
 import Heading from "@/app/components/Heading";
+import Status from "@/app/components/Status";
+import {
+  MdCached,
+  MdClose,
+  MdDelete,
+  MdDone,
+  MdRemoveRedEye,
+} from "react-icons/md";
+import ActionBtn from "@/app/components/ActionBtn";
+import { useCallback } from "react";
+import axios from "axios";
+import toast from "react-hot-toast";
+import { useRouter } from "next/navigation";
+import { deleteObject, getStorage, ref } from "firebase/storage";
+import { firebaseApp } from "@/libs/firebase";
 
 interface ManageProductsClientProps {
   products: Product[];
@@ -12,6 +27,8 @@ interface ManageProductsClientProps {
 const ManageProductsClient: React.FC<ManageProductsClientProps> = ({
   products,
 }) => {
+  const router = useRouter();
+  const storage = getStorage(firebaseApp);
   let rows: any = [];
   if (products) {
     rows = products.map((product) => {
@@ -48,7 +65,23 @@ const ManageProductsClient: React.FC<ManageProductsClientProps> = ({
       width: 120,
       renderCell: (params) => {
         return (
-          <div>{params.row.inStock === true ? "in stock" : "out of stock"}</div>
+          <div>
+            {params.row.inStock === true ? (
+              <Status
+                text="in stock"
+                icon={MdDone}
+                bg="bg-teal-200"
+                color="text-teal-700"
+              />
+            ) : (
+              <Status
+                text="out of stock"
+                icon={MdClose}
+                bg="bg-rose-200"
+                color="text-rose-700"
+              />
+            )}
+          </div>
         );
       },
     },
@@ -57,17 +90,91 @@ const ManageProductsClient: React.FC<ManageProductsClientProps> = ({
       headerName: "Actions",
       width: 200,
       renderCell: (params) => {
-        return <div>Action</div>;
+        return (
+          <div className="flex justify-between gap-4 w-full">
+            <ActionBtn
+              icon={MdCached}
+              onClick={() =>
+                handleToggleStock(params.row.id, params.row.inStock)
+              }
+            />
+            <ActionBtn
+              icon={MdDelete}
+              onClick={() => {
+                handleDelete(params.row.id, params.row.images);
+              }}
+            />
+            <ActionBtn
+              icon={MdRemoveRedEye}
+              onClick={() => {
+                router.push(`product/${params.row.id}`);
+              }}
+            />
+          </div>
+        );
       },
     },
   ];
+
+  const handleToggleStock = useCallback(
+    (id: string, inStock: boolean) => {
+      const loadingToast = toast.loading("Status Updating...");
+      axios
+        .put("/api/product", {
+          id,
+          inStock: !inStock,
+        })
+        .then((res) => {
+          toast.dismiss(loadingToast);
+          toast.success("Product status changed");
+          router.refresh();
+        })
+        .catch((err) => {
+          toast.dismiss(loadingToast);
+          toast.error("Oops! Something went wrong...");
+          console.log(err);
+        });
+    },
+    [router]
+  );
+
+  const handleDelete = useCallback(async (id: string, images: any) => {
+    const loadingToast = toast.loading("Deleting product, please wait!");
+
+    const handleImageDelete = async () => {
+      try {
+        for (const item of images) {
+          if (item.image) {
+            const imageRef = ref(storage, item.image);
+            await deleteObject(imageRef);
+          }
+        }
+      } catch (error) {
+        return console.log("Deleting images error", error);
+      }
+    };
+
+    await handleImageDelete();
+
+    axios
+      .delete(`/api/product/${id}`)
+      .then((res) => {
+        toast.dismiss(loadingToast);
+        toast.success("Product deleted");
+        router.refresh();
+      })
+      .catch((err) => {
+        toast.dismiss(loadingToast);
+        toast.error("Failed to delete product");
+      });
+  }, []);
 
   return (
     <div className="max-w-[1150px] m-auto text-xl">
       <div className="mb-4 mt-8">
         <Heading title="Manage Products" center />
       </div>
-      <div style={{height: 600, width: '100%'}}>
+      <div style={{ height: 600, width: "100%" }}>
         <DataGrid
           rows={rows}
           columns={columns}
@@ -78,6 +185,7 @@ const ManageProductsClient: React.FC<ManageProductsClientProps> = ({
           }}
           pageSizeOptions={[9, 20]}
           checkboxSelection
+          disableRowSelectionOnClick
         />
       </div>
     </div>
